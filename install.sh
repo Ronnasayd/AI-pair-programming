0a4558a77dd97eac7e6e5404dc10f5a24cd86bf9
Ronnasayd <ronnasayd@hotmail.com>
2026-03-07 14:18:40 -0300

    chore: reorganize install.sh script for better structure and clarity

--- install.sh
+++ install.sh
@@ -11,25 +11,34 @@ rm -rf $HOME/.gemini/commands/
 fi
 mkdir -p $HOME/.gemini/commands
 ln -s "$SOURCE/gemini/commands/"* "$HOME/.gemini/commands/"
-
+#######################################################################################
+if [ -L "$HOME/.gemini/skills/" ] || [ -d "$HOME/.gemini/skills/" ]; then
+rm -rf $HOME/.gemini/skills/
+fi
+mkdir -p $HOME/.gemini/skills
+ln -s "$SOURCE/skills/"* "$HOME/.gemini/skills/"
+########################################################################################
 # Installs for github
 if [ -L "$LOCAL/.github/skills/" ] || [ -d "$LOCAL/.github/skills/" ]; then
 rm -rf $LOCAL/.github/skills/
 fi
 mkdir -p $LOCAL/.github/skills/
 ln -s "$SOURCE/skills/"* "$LOCAL/.github/skills/"
-
-
+########################################################################################
 if [ -L "$LOCAL/.github/prompts/" ] || [ -d "$LOCAL/.github/prompts/" ]; then
 rm -rf $LOCAL/.github/prompts/
 fi
 mkdir -p $LOCAL/.github/prompts/
-ln -s "$SOURCE/.github/prompts/"* "$LOCAL/.github/prompts/"
-
-
-
+ln -s "$SOURCE/github/prompts/"* "$LOCAL/.github/prompts/"
+########################################################################################3
 if [ -L "$LOCAL/.github/instructions/copilot.instructions.md" ] || [ -f "$LOCAL/.github/instructions/copilot.instructions.md" ]; then
 rm -rf $LOCAL/.github/instructions/copilot.instructions.md
 fi
 mkdir -p $LOCAL/.github/instructions/
 ln -s "$SOURCE/instructions/copilot.instructions.md" "$LOCAL/.github/instructions/copilot.instructions.md"
+##########################################################################################
+if [ -L "$HOME/.config/Code/User/mcp.json" ] || [ -f "$HOME/.config/Code/User/mcp.json" ]; then
+rm -rf "$HOME/.config/Code/User/mcp.json"
+fi
+ln -s "$SOURCE/mcps/vscode.mcp.json" "$HOME/.config/Code/User/mcp.json"
+###########################################################################################
